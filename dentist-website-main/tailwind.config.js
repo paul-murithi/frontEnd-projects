35b936e880f42d10fa4dc2ff7726bd2f6043d08d
paul-murithi <paulmurithi233@gmail.com>
2025-02-18 15:06:33 +0300

    Add color pallete and Nav icons

--- dentist-website-main/tailwind.config.js
+++ dentist-website-main/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", // Ensure this is correct
-    "./src/**/*.{js,ts,jsx,tsx}", // Ensure your JSX/TSX files are being picked up
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stroke: "#C2D1FF",
+        buttonBG: "#3366FF",
+        buttonBGHover: "#2858D1",
+        linkHover: "#3366FF",
+        navBG: "#C7F0FD",
+        heading: "#142349",
+        text: "#142349",
+        textLight: "#142349",
+        background: "#EFF6FF",
+        subheading: "#FF9140",
+        buttonText: "#FFFFFF",
+        border: "#A5B8FF",
+        buttonTextHover: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
 };
