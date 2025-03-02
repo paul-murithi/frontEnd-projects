0b221a9a8b4f0c781de8b450b13177abc452fa50
paul-murithi <paulmurithi233@gmail.com>
2025-03-02 13:16:15 +0300

    feat: Added doctor's potrait

--- dentist-website-main/src/components/Hero/DoctorCard.jsx
+++ dentist-website-main/src/components/Hero/DoctorCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Image from "../Image/Image";
 import shieldLogo from "../../assets/images/Item-01.png";
-import potrait from "../../assets/images/doctor-potrait-removebg-preview.png";
+import doctor_potrait from "../../assets/images/doctor-potrait-removebg-preview.png";
 import floating_badge_1 from "../../assets/images/Item-08.png";
 import floating_badge_2 from "../../assets/images/Item-03.png";
 import floating_badge_3 from "../../assets/images/Item-04.png";
 
 const DoctorCard = () => {
   return (
-    <div className="bg-background">
+    <div className="bg-background pb-14">
       <div className="flex justify-center items-center relative">
         {/**Outer most circle */}
         <div className="w-[30rem] h-[30rem] rounded-full border-4 border-dotted border-gray-300 flex justify-center items-center relative">
@@ -26,10 +26,14 @@ const DoctorCard = () => {
             />
             {/**Third circle)*/}
             <div className="w-[18rem] h-[18rem] rounded-full flex justify-center items-center bg-blue-700 relative">
+              <Image
+                source={doctor_potrait}
+                className="absolute bottom-0 w-60"
+              />
               {/**Fourth circle */}
               <div className="w-[12rem] h-[12rem] rounded-full flex justify-center items-center bg-blue-600">
                 {/**Inner circle */}
-                <div className="w-[6rem] h-[6rem] rounded-full bg-blue-500"></div>
+                <div className="w-[6rem] h-[6rem] rounded-full bg-blue-500 "></div>
               </div>
             </div>
           </div>
