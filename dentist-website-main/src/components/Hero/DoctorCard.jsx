9d8f26a38aac2cf2feecb6367fac04456802f16d
paul-murithi <paulmurithi233@gmail.com>
2025-03-02 12:52:34 +0300

    feat: Added floating badges

--- dentist-website-main/src/components/Hero/DoctorCard.jsx
+++ dentist-website-main/src/components/Hero/DoctorCard.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import Image from "../Image/Image";
+import shieldLogo from "../../assets/images/Item-01.png";
+import potrait from "../../assets/images/doctor-potrait-removebg-preview.png";
+import floating_badge_1 from "../../assets/images/Item-08.png";
+import floating_badge_2 from "../../assets/images/Item-03.png";
+import floating_badge_3 from "../../assets/images/Item-04.png";
 
 const DoctorCard = () => {
   return (
     <div className="bg-background">
-      <div className="flex justify-center items-center">
-        <div className="w-[30rem] h-[30rem] rounded-full border-4 border-dotted border-gray-300 flex justify-center items-center">
+      <div className="flex justify-center items-center relative">
+        {/**Outer most circle */}
+        <div className="w-[30rem] h-[30rem] rounded-full border-4 border-dotted border-gray-300 flex justify-center items-center relative">
+          <Image source={shieldLogo} className="absolute top-1 left-12" />
+          <Image
+            source={floating_badge_2}
+            className="absolute -right-36 z-10"
+          />
+          {/**Second circle */}
           <div className="w-[24rem] h-[24rem] rounded-full border-4 border-dotted border-gray-300 flex justify-center items-center">
-            <div className="w-[18rem] h-[18rem] rounded-full flex justify-center items-center bg-blue-700">
+            <Image source={floating_badge_1} className="absolute left-0 z-10" />
+            <Image
+              source={floating_badge_2}
+              className="absolute left-12 z-10 bottom-12"
+            />
+            {/**Third circle)*/}
+            <div className="w-[18rem] h-[18rem] rounded-full flex justify-center items-center bg-blue-700 relative">
+              {/**Fourth circle */}
               <div className="w-[12rem] h-[12rem] rounded-full flex justify-center items-center bg-blue-600">
+                {/**Inner circle */}
                 <div className="w-[6rem] h-[6rem] rounded-full bg-blue-500"></div>
               </div>
             </div>
