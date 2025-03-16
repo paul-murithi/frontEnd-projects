c4f146a0c5eefdacede264e574fd9fb6674eed6f
paul-murithi <paulmurithi233@gmail.com>
2025-03-16 16:59:13 +0300

    feat: Add floating badge on doctor's potrait

--- dentist-website-main/src/components/About/DoctorPotrait.jsx
+++ dentist-website-main/src/components/About/DoctorPotrait.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 import nursePotrait from "../../assets/images/doctor-potrait-pg-2.png";
+import floatingBadge from "../../assets/images/Item-08.png";
+import floatingBadgeRight from "../../assets/images/Group-19.png";
 import Image from "../Image/Image";
 
 const DoctorPotrait = () => {
   return (
     <div className="w-full flex justify-center items-center my-[10%] relative">
-      <div className="rounded-lg w-3/5 h-[26rem] border relative z-30 flex items-center justify-center overflow-hidden">
+      <Image source={floatingBadge} className="absolute z-30 left-8" />
+      <div className="shadow-2xl z-30 absolute bottom-2 right-16 p-2">
+        <Image source={floatingBadgeRight} className="" />
+      </div>
+
+      <div className="rounded-lg w-3/5 h-[26rem] border relative z-10 flex items-center justify-center overflow-hidden">
         {/* Circles */}
         <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center">
           <div className="absolute rounded-full w-[100vw] h-[100vw] bg-blue-100"></div>
@@ -21,7 +28,7 @@ const DoctorPotrait = () => {
 
       <>
         {/* Image */}
-        <Image source={nursePotrait} className="absolute bottom-0 z-40" />
+        <Image source={nursePotrait} className="absolute bottom-0 z-20" />
       </>
     </div>
   );
